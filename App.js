4c72eb9689da628bdaaa8d5b6843b91120666d7a
Juan <a1120150025@utch.edu.mx>
2022-03-07 12:56:42 -0700

    Actividad 8 Diseño de Apps

--- App.js
+++ App.js
@@ -1,13 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { StyleSheet, Text, View, FlatList, ActivityIndicator} from 'react-native';
 
 export default function App() {
+    const [datos, setDatos] = useState([]);
+    const [isLoading, setLoading] = useState([]);
+
+  
+  const getPosts = async() => {
+    try{
+    const url = "https://jsonplaceholder.typicode.com/posts";
+
+    //consumir los datos
+    const response = await fetch(url);
+    //convertir a json
+    const json = await response.json();
+    setDatos(json);
+    }catch (error){
+      console.error(error);
+    } finally{
+      setLoading(false);
+    }
+
+  }
+  
+
+  useEffect(()=>{
+    getPosts();
+  }, [])
+
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+     {
+      isLoading ?  <ActivityIndicator /> : (
+
+      <FlatList 
+            data={datos} 
+            keyExtractor={ ( { id }, index) => id } 
+            renderItem = {
+            ({item}) => (
+            <Text> {item.title} </Text>
+            )
+          }
+        /> 
+      )
+     }
+
     </View>
   );
+
 }
 
 const styles = StyleSheet.create({
